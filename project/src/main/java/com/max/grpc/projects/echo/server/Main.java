d1954846160a608e7035924b2e3ff239f2f1124c
Maksym Hubach <mhuba@softserveinc.com>
2020-02-18 17:36:52 +0200

    EchoServer: removed redundant type declaration and `final` modifier

--- project/src/main/java/com/max/grpc/projects/echo/server/Main.java
+++ project/src/main/java/com/max/grpc/projects/echo/server/Main.java
@@ -3,7 +3,7 @@ package com.max.grpc.projects.echo.server;
 
 public class Main {
     public static void main(String[] args) {
-        final EchoServer server = new EchoServer();
+        var server = new EchoServer();
         server.start();
         server.blockUntilShutdown();
     }
